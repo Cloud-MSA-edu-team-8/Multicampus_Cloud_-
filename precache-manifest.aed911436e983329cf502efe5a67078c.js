75136c3ce4226afea1061383abf937c83205a4b5
zzid <931010dy@gmail.com>
2020-08-28 17:43:49 +0900

    Updates

---
+++ precache-manifest.aed911436e983329cf502efe5a67078c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2fe6f50c3bec0bc97ac9e8ffc8b9a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75430a8deb8b4544cbcd",
+    "url": "/static/css/main.77d1eef5.chunk.css"
+  },
+  {
+    "revision": "4bf1ebe1efa4d9890313",
+    "url": "/static/js/2.700c6826.chunk.js"
+  },
+  {
+    "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
+    "url": "/static/js/2.700c6826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75430a8deb8b4544cbcd",
+    "url": "/static/js/main.4f50e8cf.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);

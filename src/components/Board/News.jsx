435e7138f400140b65a75764c022e592e1897a97
zzid <931010dy@gmail.com>
2020-08-29 13:32:27 +0900

    Add link icon on news

--- src/components/Board/News.jsx
+++ src/components/Board/News.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import styles from './News.module.css';
 import Loading from '../Loading/Loading';
+import linkIcon from '../../images/link.png';
 import { makeStyles } from '@material-ui/core/styles';
 import { List, Typography, ListItemText, ListItem, Divider } from '@material-ui/core';
 
@@ -21,35 +22,29 @@ const News = ({newsData}) =>{
             <Typography variant="h3" align="center">News</Typography>
             <List className={styles.list}>
                 {newsData.map(data=>(
-                <a className={styles.a}href={data.link} target="_blank">
-                    <ListItem className={styles.liItem}>
-                        <ListItemText
-                        primary={data.title}
-                        primaryTypographyProps={{component:"li", variant:"body1"}}
-                        secondary={
-                                <Typography
-                                    variant="body2"
-                                    color="textSecondary"
-                                >
-                                {data.contents}
-                                </Typography>
-                        }
-                        />
-                    </ListItem>
-                    <Divider variant="fullWidth" component="li" />
-                </a>
+                    <a className={styles.a} href={data.link} target="_blank">
+                        <ListItem className={styles.liItem}>
+                            <ListItemText
+                            primary={
+                                <>
+                                {data.title+' '}<img src={linkIcon} width="15" height="15"/>
+                                </>}
+                            primaryTypographyProps={{component:"li", variant:"body1"}}
+                            secondary={
+                                    <Typography
+                                        variant="body2"
+                                        color="textSecondary"
+                                    >
+                                    {data.contents}
+                                    </Typography>
+                            }
+                            />
+                        </ListItem>
+                        <Divider variant="fullWidth" component="li" />
+                    </a>
                 ))}
             </List>
         </div>
     )
 }
-
-        {/* <div className={styles.container}>
-            <h1>News</h1>
-            <ul className={styles.news}>
-            {newsData.map(data=>(
-                    <li className={styles.li}><a href={data.link} target='_blank'><h4>{data.title}</h4></a></li>
-            ))}
-            </ul>
-        </div> */}
 export default News

3cdffa5c3a568e8209c69ad38adf95575cc49fc4
zzid <931010dy@gmail.com>
2020-09-02 14:40:43 +0900

    Updates

--- precache-manifest.c3ddd2f35792cd61dda6a63a1562d7d2.js
+++ precache-manifest.c3ddd2f35792cd61dda6a63a1562d7d2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "38d3771d6f4b53706f7c4c45052461a1",
+    "revision": "4c098385d24034cb033085b64727b2cb",
     "url": "/safehome_frontend/index.html"
   },
   {
-    "revision": "b9f1c5ff223a892e4450",
-    "url": "/safehome_frontend/static/css/main.898d23a9.chunk.css"
+    "revision": "3bd1719429194ddb37c8",
+    "url": "/safehome_frontend/static/css/main.b71cd28b.chunk.css"
   },
   {
-    "revision": "b73863760f55a5be4020",
-    "url": "/safehome_frontend/static/js/2.a9d0ad37.chunk.js"
+    "revision": "b406429b707ff54c3f6d",
+    "url": "/safehome_frontend/static/js/2.700a051f.chunk.js"
   },
   {
     "revision": "32aeafb3e2d0a73f27822c8ed8045b88",
-    "url": "/safehome_frontend/static/js/2.a9d0ad37.chunk.js.LICENSE.txt"
+    "url": "/safehome_frontend/static/js/2.700a051f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b9f1c5ff223a892e4450",
-    "url": "/safehome_frontend/static/js/main.89b64b78.chunk.js"
+    "revision": "3bd1719429194ddb37c8",
+    "url": "/safehome_frontend/static/js/main.edd573b0.chunk.js"
   },
   {
     "revision": "b92a07e657b343548d85",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/safehome_frontend/precache-manifest.33db22747fdf6710f298031046075022.js"
+  "/safehome_frontend/precache-manifest.c3ddd2f35792cd61dda6a63a1562d7d2.js"
 );
 
 self.addEventListener('message', (event) => {
